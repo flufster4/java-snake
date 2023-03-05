8f18da3f96114cf8715204a7fc12be9dc1005846
Markian <markian.vrublevsky@gmail.com>
2023-03-05 16:54:08 -0600

    fixed snake jumping bug, added death messages

--- src/main/java/com/guinea/Main.java
+++ src/main/java/com/guinea/Main.java
@@ -1,9 +1,12 @@
 package com.guinea;
 
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         MainWindow window = new MainWindow();
+        window.repaint();
     }
 }
--- src/main/java/com/guinea/MainWindow.java
+++ src/main/java/com/guinea/MainWindow.java
@@ -1,9 +1,16 @@
 package com.guinea;
 
-import com.guinea.game.*;
+import com.guinea.game.Direction;
+import com.guinea.game.Drawable;
+import com.guinea.game.GameColors;
+import com.guinea.game.Grid;
 import com.guinea.game.snake.Snake;
 import com.guinea.game.snake.SnakeType;
 import com.guinea.geomatry.MatrixUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -13,6 +20,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 import java.util.Timer;
@@ -22,6 +31,7 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
 
     private TimerTask snakeTask;
     public Timer gameloopTimer = new Timer("gameloop");
+    public Timer bgTimer = new Timer("background");
     public int[][] gameBoardMatrix = new int[10][10];
     private final Random random = new Random();
     public Snake snake = new Snake(5, 5, SnakeType.NORMAL);
@@ -30,9 +40,7 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
 
     private static final Object moveLock = new Object();
     private boolean isDead = false;
-
-    private int snakeMoveDelay = 500;
-    private int snakeMoveDelaySnapshot = snakeMoveDelay;
+    private final int snakeMoveDelay = 500;
     private Direction moveIn = Direction.RIGHT;
     private final BufferedImage normalHead = ImageIO.read(new File("D:\\java-projects\\Snake\\src\\main\\resources\\snake\\flushed.png"));
     private final BufferedImage fatHead = ImageIO.read(new File("D:\\java-projects\\Snake\\src\\main\\resources\\snake\\fat.png"));
@@ -40,8 +48,10 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
     private final BufferedImage madHead = ImageIO.read(new File("D:\\java-projects\\Snake\\src\\main\\resources\\snake\\mad.png"));
     private final BufferedImage apple = ImageIO.read(new File("D:\\java-projects\\Snake\\src\\main\\resources\\apple.png"));
     private boolean isMoveCanceled = false;
+    Object msgJson = new JSONParser().parse(new FileReader("D:\\java-projects\\Snake\\src\\main\\resources\\deathmsg.json"));
+    private String deathMessage;
 
-    MainWindow() throws IOException {
+    MainWindow() throws IOException, ParseException {
         super("Snake+");
         setSize(520, 550);
         requestFocus();
@@ -59,48 +69,28 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
             }
         });
 
-        //this task is used to update the GUI 10 times per second
-        TimerTask updateTask = new TimerTask() {
-            @Override
-            public void run() {
-                synchronized (moveLock) {
-                    repaint();
-                }
-            }
-        };
         snakeTask = new TimerTask() {
             @Override
             public void run() {
                 updateSnake();
             }
         };
-        TimerTask snakeBgTask = new TimerTask() {
+
+        TimerTask abilityTask = new TimerTask() {
             @Override
             public void run() {
-                int changeType = random.nextInt(20);
-                if ((changeType == 11) && (MatrixUtils.distance(snake.getSection(0).getX(), snake.getSection(0).getY(), appleX, appleY) > 2))
-                    snake.setType(SnakeType.FAT);
-                else if (changeType == 4) snake.setType(SnakeType.EVIL);
-                else if (changeType == 19 && !(((snake.getSection(0).getX() >= 7) || (snake.getSection(0).getX() <= 2)) ||
-                        ((snake.getSection(0).getY() >= 7) || (snake.getSection(0).getY() <= 2)) && snake.getType().equals(SnakeType.RAM)))
-                    snake.setType(SnakeType.RAM);
-                else if (!snake.getType().equals(SnakeType.RAM)) {
-                    snake.setType(SnakeType.NORMAL);
-                    snakeMoveDelay = snakeMoveDelaySnapshot;
-                }
-
-                snakeMoveDelaySnapshot = snakeMoveDelay;
-                if (snake.getType().equals(SnakeType.RAM)) snakeMoveDelay = 250;
-                else snakeMoveDelay = snakeMoveDelay * 2;
+                int num = random.nextInt(10);
+                if (num == 2 && MatrixUtils.distance(snake.getSection(0).getX(),snake.getSection(0).getY(),appleX,appleY) > 2) snake.setType(SnakeType.FAT);
+                if (num == 7 && snake.getSection(0).getX() > 0 && snake.getSection(0).getY() > 0 && snake.getSection(0).getX() < 9 && snake.getSection(0).getY() < 9) snake.setType(SnakeType.EVIL);
+                else snake.setType(SnakeType.NORMAL);
             }
         };
 
         generateApple();
-        //schedule gameloop tasks
-        gameloopTimer.schedule(updateTask, 0, 10);
-        gameloopTimer.schedule(snakeTask, 0, snakeMoveDelay);
 
-        //gameloopTimer.schedule(snakeBgTask, 1000,1000);
+        //schedule gameloop tasks
+        gameloopTimer.schedule(snakeTask, 0, snakeMoveDelay);
+        bgTimer.schedule(abilityTask, 0, snakeMoveDelay);
 
         //init matrix
         for (int i = 0; i < 10; i++) {
@@ -188,23 +178,41 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
 
         if (isDead) {
             gameloopTimer.cancel();
+
+            JSONObject jo = (JSONObject) msgJson;
+            JSONArray normalMessages = (JSONArray) jo.get("normal_messages");
+            JSONArray fatMessages = (JSONArray) jo.get("fat_messages");
+            JSONArray evilMessages = (JSONArray) jo.get("evil_messages");
+
+            switch (snake.getType()) {
+                case FAT:
+                    int rand = random.nextInt(fatMessages.size());
+                    deathMessage = (String) fatMessages.get(rand);
+                    break;
+                case EVIL:
+                    int rand1 = random.nextInt(evilMessages.size());
+                    deathMessage = (String) evilMessages.get(rand1);
+                    break;
+                case NORMAL:
+                    int rand2 = random.nextInt(normalMessages.size());
+                    deathMessage = (String) normalMessages.get(rand2);
+                    break;
+            }
+
             g.setColor(new Color(36, 33, 32, 127));
             g.fillRect(0, 0, 600, 600);
             g.setColor(Color.WHITE);
             g.setFont(new Font("", Font.BOLD, 40));
             g.drawString("GAME OVER", 140, 275);
             g.setFont(new Font("", Font.BOLD, 20));
-            g.drawString("SCORE: " + (snake.getLength() - 1), 140, 300);
+            g.drawString("SCORE: " + (snake.getLength() - 1), 8, 50);
             g.drawString("PRESS ENTER TO RESTART", 235, 50);
+            g.drawString(deathMessage, 140,300);
         }
     }
 
-    @Override
-    public void update(Graphics g) {
-        paint(g);
-    }
-
     public void updateSnake() {
+
         synchronized (moveLock) {
             if (isMoveCanceled) {
                 isMoveCanceled = false;
@@ -223,27 +231,15 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
 
                 //ram snake
                 if (((snake.getSection(0).getX() >= 7) || (snake.getSection(0).getX() <= 2)) ||
-                        ((snake.getSection(0).getY() >= 7) || (snake.getSection(0).getY() <= 2)) && snake.getType().equals(SnakeType.RAM)) {
-                    snake.setType(SnakeType.NORMAL);
-                }
+                        ((snake.getSection(0).getY() >= 7) || (snake.getSection(0).getY() <= 2)) && snake.getType().equals(SnakeType.RAM))
+                            snake.setType(SnakeType.NORMAL);
+
 
                 //die
-                try {
-                    Cordinates nextCords = snake.getNextCords(moveIn);
-                    System.out.println("nextCords = " + nextCords);
-                    if (gameBoardMatrix[nextCords.getX()][nextCords.getY()] == 2) {
-                        System.out.println("Old Matrix:");
-                        for (int i = 0; i < 10; i++) {
-                            for (int j = 0; j < 10; j++) {
-                                System.out.print(gameBoardMatrix[i][j]);
-                            }
-                        }
-                        System.out.println("\nDirection: "+moveIn);
-                        die("SELF_INFLICTED_DEATH");
-                    }
-                } catch (IndexOutOfBoundsException e) {
-                    die("WALL!!");
-                }
+                for (int i = 0; i < snake.getLength() - 1; i++)
+                    if (snake.getSection(i).isHead())
+                        if (gameBoardMatrix[snake.getSection(i).getX()][snake.getSection(i).getY()] == 2)
+                            die("yourself");
 
                 //reset board
                 resetMatrix();
@@ -253,29 +249,16 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
                 snake.addSection(moveIn, removeTail);
 
                 //update game board
-                for (int i = 0; i < snake.getLength() - 1; i++) {
+                for (int i = 0; i < snake.getLength() - 1; i++)
                     if (snake.getSection(i).isHead()) {
                         try {
                             gameBoardMatrix[snake.getSection(i).getX()][snake.getSection(i).getY()] = 3;
                         } catch (IndexOutOfBoundsException e) {
                             die("WALL AGAIN!!");
                         }
-                    } else {
-                        gameBoardMatrix[snake.getSection(i).getX()][snake.getSection(i).getY()] = 2;
-                    }
-                }
+                    } else gameBoardMatrix[snake.getSection(i).getX()][snake.getSection(i).getY()] = 2;
 
-                System.out.println("Matrix:");
-                for (int i = 0; i < 10; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        System.out.print(gameBoardMatrix[i][j]);
-                    }
-                }
-                System.out.println("\nDirection: "+moveIn);
-            } catch (DiedException de) {
-                System.out.println(Thread.currentThread().getName() + ">> Died of " + de.getMessage());
             } finally {
-                System.out.println(Thread.currentThread().getName() + ">> repainted!");
                 repaint();
             }
         }
@@ -312,57 +295,69 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
             if (snake.getType().equals(SnakeType.EVIL))
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_UP:
-                        if (!moveIn.equals(Direction.UP)) moveIn = Direction.DOWN;
+                        if (!moveIn.equals(Direction.UP))
+                            changeMove(Direction.DOWN);
                         break;
                     case KeyEvent.VK_DOWN:
-                        if (!moveIn.equals(Direction.DOWN)) moveIn = Direction.UP;
+                        if (!moveIn.equals(Direction.DOWN))
+                            changeMove(Direction.UP);
                         break;
                     case KeyEvent.VK_LEFT:
-                        if (!moveIn.equals(Direction.LEFT)) moveIn = Direction.RIGHT;
+                        if (!moveIn.equals(Direction.LEFT))
+                            changeMove(Direction.RIGHT);
                         break;
                     case KeyEvent.VK_RIGHT:
-                        if (!moveIn.equals(Direction.RIGHT)) moveIn = Direction.LEFT;
+                        if (!moveIn.equals(Direction.RIGHT))
+                            changeMove(Direction.LEFT);
                 }
             else if (!snake.getType().equals(SnakeType.RAM))
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_UP:
-                        if (!moveIn.equals(Direction.DOWN)) {
-                            moveIn = Direction.UP;
-                            changeMove();
-                        }
+                        if (!moveIn.equals(Direction.DOWN))
+                            changeMove(Direction.UP);
                         break;
                     case KeyEvent.VK_DOWN:
-                        if (!moveIn.equals(Direction.UP)) {
-                            moveIn = Direction.DOWN;
-                            changeMove();
-                        }
+                        if (!moveIn.equals(Direction.UP))
+                            changeMove(Direction.DOWN);
                         break;
                     case KeyEvent.VK_LEFT:
-                        if (!moveIn.equals(Direction.RIGHT)) {
-                            moveIn = Direction.LEFT;
-                            changeMove();
-                        }
+                        if (!moveIn.equals(Direction.RIGHT))
+                            changeMove(Direction.LEFT);
                         break;
                     case KeyEvent.VK_RIGHT:
-                        if (!moveIn.equals(Direction.LEFT)) {
-                            moveIn = Direction.RIGHT;
-                            changeMove();
-                        }
+                        if (!moveIn.equals(Direction.LEFT))
+                            changeMove(Direction.RIGHT);
                         break;
                 }
             if (isDead && e.getKeyCode() == KeyEvent.VK_ENTER) {
                 this.isDead = false;
                 generateApple();
+                this.gameloopTimer = new Timer("gameloop");
+                this.snakeTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        updateSnake();
+                    }
+                };
+                this.gameloopTimer.schedule(snakeTask, 0,snakeMoveDelay);
                 this.snake = new Snake(5, 5, SnakeType.NORMAL);
                 this.moveIn = Direction.RIGHT;
             }
         }
     }
 
-    private void changeMove() {
-        if (isMoveCanceled) return;
-        System.out.println(Thread.currentThread().getName() + ">> moveIn = " + moveIn);
+    private void changeMove(Direction newDirection) {
+        moveIn = newDirection;
         isMoveCanceled = true;
+        snakeTask.cancel();
+        gameloopTimer.purge();
+        snakeTask = new TimerTask() {
+            @Override
+            public void run() {
+                updateSnake();
+            }
+        };
+        gameloopTimer.schedule(snakeTask, 0,snakeMoveDelay);
         updateSnake();
     }
 
@@ -374,4 +369,5 @@ public class MainWindow extends DoubleBuffer implements KeyListener {
     public boolean isDoubleBuffered() {
         return true;
     }
+
 }
